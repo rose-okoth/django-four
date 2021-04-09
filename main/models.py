5f9ee5d01d453fca47bda699ec565c423fdb99f0
rose-okoth <okoth.rose0@gmail.com>
2021-04-09 16:30:42 +0300

    added neighborhood

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib import admin
+
 
 # Create your models here.
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=250)
+    location = models.CharField(max_length=250)
+    occupants = models.CharField(max_length=250)
+    # admin = models.ForeignKey(admin, on_delete=models.CASCADE)
