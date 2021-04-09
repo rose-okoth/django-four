bbfef48695cb1b69f1b224c9264c3327ce02b840
rose-okoth <okoth.rose0@gmail.com>
2021-04-09 15:39:07 +0300

    fixed authentication bug

--- accounts/email.py
+++ accounts/email.py
@@ -3,12 +3,12 @@ from django.template.loader import render_to_string
 
 def send_welcome_email(name,receiver):
     # Creating message subject and sender
-    subject = 'Welcome to The Neighborhood Watch!'
+    subject = 'Welcome to HoodWatch!'
     sender = 'schoolprojcts3@gmail.com'
 
     #passing in the context vairables
-    text_content = render_to_string('welcome.txt',{"name": name})
-    html_content = render_to_string('welcome.html',{"name": name})
+    text_content = render_to_string('email/welcome.txt',{"name": name})
+    html_content = render_to_string('email/welcome.html',{"name": name})
 
     msg = EmailMultiAlternatives(subject,text_content,sender,[receiver])
     msg.attach_alternative(html_content,'text/html')
