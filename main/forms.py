1175d72af7751d5a6da7bc48d218f4a76ee75892
rose-okoth <okoth.rose0@gmail.com>
2021-04-10 10:51:38 +0300

    added the profile update form

--- main/forms.py
+++ main/forms.py
@@ -5,3 +5,15 @@ class NeighborhoodForm(forms.ModelForm):
     class Meta:
         model = Neighborhood
         fields = ('name', 'location', 'occupants')
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name','email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image', 'bio']
