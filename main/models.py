03de64a4ac55a64614a4886683292059948a4fd6
rose-okoth <okoth.rose0@gmail.com>
2021-04-12 23:47:07 +0300

    modified details page

--- main/models.py
+++ main/models.py
@@ -35,15 +35,6 @@ class Neighborhood(models.Model):
     def get_absolute_url(self):
         return reverse("main:detail", kwargs={"slug": self.slug})
 
-    def save(self, *args, **kwargs):
-        super(Profile, self).save(*args, **kwargs)
-
-        img = Image.open(self.image.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
 
 
 def create_slug(instance, new_slug=None):
