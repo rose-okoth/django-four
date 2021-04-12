978ba38a81e984006a511600358611fb989752d1
rose-okoth <okoth.rose0@gmail.com>
2021-04-13 00:46:21 +0300

    user test case

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,75 @@
 from django.test import TestCase
+from .models import *
+import datetime as dt
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class UserTestClass(TestCase):
+
+# Set up method
+    def setUp(self):
+        self.rose= User(username = 'Rose', password = '123456789')
+
+# Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.rose,User))
+
+# class ProjectTestClass(TestCase):
+    
+#     def setUp(self):
+#         # Creating a new user and saving it
+#         self.rose= user(first_name = 'Rose', last_name ='Okoth', email ='okoth.rose@gmail.com')
+#         self.rose.save_user()
+
+#         # Creating a new tag and saving it
+#         self.new_slug = slug(name = 'testing')
+#         self.new_slug.save()
+#         self.new_post= Post(title = 'Test Post',content = 'This is a random test',editor = self.rose)
+#         self.new_post.save()
+#         self.new_post.slug.add(self.new_slug)
+
+#         def tearDown(self):
+#             User.objects.all().delete()
+#             Post.objects.all().delete()
+
+# class TestProfile(TestCase):
+#     def setUp(self):
+#         self.user = User(username='mimi', password='passw0rd')
+#         self.user.save()
+
+#     def test_instance(self):
+#         self.assertTrue(isinstance(self.user, User))
+
+#     def test_save_user(self):
+#         self.user.save()
+
+#     def test_delete_user(self):
+#         self.user.delete()
+
+
+# class ProjectTest(TestCase):
+#     def setUp(self):
+#         self.user = User.objects.create(username='mimi')
+#         self.project = Project.objects.create(title='project', image='https://picture.com', description='description', link='http://url.com')
+
+#     def test_instance(self):
+#         self.assertTrue(isinstance(self.project, Project))
+
+#     def test_save_project(self):
+#         self.project.save_project()
+#         project = Project.objects.all()
+#         self.assertTrue(len(project) > 0)
+
+#     def test_get_projects(self):
+#         self.project.save()
+#         projects = Project.all_projects()
+#         self.assertTrue(len(projects) > 0)
+
+#     def test_search_project(self):
+#         self.project.save()
+#         project = Project.search_project('test')
+#         self.assertTrue(len(project) > 0)
+
+#     def test_delete_project(self):
+#         self.project.delete_project()
+#         project = Project.search_project('test')
+#         self.assertTrue(len(project) < 1)
